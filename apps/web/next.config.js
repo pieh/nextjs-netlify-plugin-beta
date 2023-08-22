6aa371fd550af425dceb822ad218128202448b27
Spencer Elliott <spencer@cohere.com>
2023-08-22 17:37:56 -0400

    Log NEXT_REWRITE_DOCS_URL

--- apps/web/next.config.js
+++ apps/web/next.config.js
@@ -1,6 +1,7 @@
 if (!process.env.NEXT_REWRITE_DOCS_URL) {
   throw new Error('NEXT_REWRITE_DOCS_URL environment variable is not defined');
 }
+console.info('process.env.NEXT_REWRITE_DOCS_URL', process.env.NEXT_REWRITE_DOCS_URL);
 
 module.exports = {
   reactStrictMode: true,
