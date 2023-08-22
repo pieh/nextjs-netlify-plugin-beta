fa2fe0db998f3323a2def1f1ffabbcc594e06cff
Spencer Elliott <spencer@cohere.com>
2023-08-22 16:49:21 -0400

    Configure rewrite

--- apps/web/next.config.js
+++ apps/web/next.config.js
@@ -1,4 +1,12 @@
 module.exports = {
   reactStrictMode: true,
   transpilePackages: ["ui"],
+  async rewrites() {
+    return [
+      {
+        source: '/docs/:path*',
+        destination: `https://nextjs-basepath-rewrite-issue-docs.netlify.app/:path*`,
+      },
+    ];
+  },
 };
